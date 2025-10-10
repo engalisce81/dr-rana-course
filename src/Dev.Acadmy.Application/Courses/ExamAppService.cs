fa212d541d941c4d7d6572e71455191b52fcf784
Ali Mohamed <alisce81@gmail.com>
2025-10-10 10:20:14 +0300

    exam

--- src/Dev.Acadmy.Application/Courses/ExamAppService.cs
+++ src/Dev.Acadmy.Application/Courses/ExamAppService.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Services;
 
 namespace Dev.Acadmy.Courses
 {
-    public class ExamAppService:DomainService
+    public class ExamAppService:ApplicationService
     {
         private readonly ExamManager _examManager;
         public ExamAppService(ExamManager examManager) 
